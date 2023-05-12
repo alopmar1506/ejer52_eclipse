64595c4d9aee92cbe61b019ff8e741dee0eaed57
Ana <alopmar1506@g.educaand.es>
2023-05-12 11:22:35 +0200

    Eclipse primer cambio

---
+++ src/ejer52_eclipseGit/Main.java
@@ -0,0 +1,10 @@
+package ejer52_eclipseGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola me llamo Ana");
+	}
+
+}
